dbad0ebf5eb29cdcc963ae56cbeb4ea89df3f05e
Aljon Aniban <aljon.aniban@gmail.com>
2016-07-09 21:29:59 +1000

    Initial setup for External model.

--- app/models/maestrano/connector/rails/external.rb
+++ app/models/maestrano/connector/rails/external.rb
@@ -2,29 +2,27 @@ class Maestrano::Connector::Rails::External
   include Maestrano::Connector::Rails::Concerns::External
 
   def self.external_name
-    # TODO
-    # Returns the name of the external app
+    'BaseCRM'
   end
 
   def self.get_client(organization)
-    # TODO
-    # Returns a client for the external application for the given organization
-    # e.g
-    # SomeName.new :oauth_token => organization.oauth_token,
-    # refresh_token: organization.refresh_token,
-    # instance_url: organization.instance_url,
-    # client_id: ENV[''],
-    # client_secret: ENV['']
+    BaseCRM::Client.new access_token: organization.oauth_token
   end
 
   # Return an array of all the entities that the connector can synchronize
   # If you add new entities, you need to generate
   # a migration to add them to existing organizations
   def self.entities_list
+    %w(organization person product)
     # TODO
     # The names in this list should match the names of your entities class
     # e.g %w(person, tasks_list)
     #  will synchronized Entities::Person and Entities::TasksList
     []
+  end
+
+  def self.fetch_company(organization)
+    client = self.get_client(organization)
+    client.accounts.self
   end
 end
